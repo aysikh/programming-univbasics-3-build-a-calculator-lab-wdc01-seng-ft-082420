c161d1e5ed0c27e98e2200b379055aac071b53cd
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-06 14:04:30 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,33 @@
-def addition(num1, num2)
-
+def addition(num1 = "5", num2 = "4")
+  num1 + num2
 end
+
+puts addition
 
 def subtraction(num1, num2)
-
+  num2 - num1
 end
 
-def division(num1, num2)
+subtraction("5", "10")
 
+def division(num1 = "2", num2 = "50")
+  num2 / num1
 end
+
+puts division
 
 def multiplication(num1, num2)
-
+  num1*num2
 end
+
+puts multiplication
 
 def modulo(num1, num2)
 
 end
 
-def square_root(num)
-
+def square_root(num = "81")
+  Math.sqrt(num)
 end
+
+puts square_root
