0a61f8fa73f9e2f4634d401102c466c3d9cd226e
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-06 14:39:15 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,17 @@
 def addition(num1, num2)
-  sum = "#{num2} + #{num1}"
+  sum = num2 + num1
   return sum
 end
 
 
 def subtraction(num1, num2)
-  sum = "#{num2} - #{num1}"
+  sum = #
   return sum
 end
 
 
 def division(num1, num2)
-  sum = "#{num2}/#{num1}" 
+  sum = #{num2}/#{num1} 
 end
 
 def multiplication(num1, num2)
