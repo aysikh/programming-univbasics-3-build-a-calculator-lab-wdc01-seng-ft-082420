b4fdb76f74ef225cbb287aeb1187ba435c606355
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-06 14:45:56 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,13 +5,13 @@ end
 
 
 def subtraction(num1, num2)
-  sum = num2 - num1
+  sum = num1 - num2
   return sum
 end
 
 
 def division(num1, num2)
-  sum = num2 / num1
+  sum = num2/num1
   return sum
 end
 
